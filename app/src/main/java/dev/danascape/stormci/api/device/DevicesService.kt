35dbea93f0afec83c4679336dc0652fbe9ee86b1
danascape <danascape@gmail.com>
2022-11-13 16:03:09 +0530

    StormCI: Convert to Coroutines in device fragment (1)
    * In response to https://github.com/danascape/StormCI/issues/5
    * More work will be done in the upcoming commits.

--- app/src/main/java/dev/danascape/stormci/api/device/DevicesService.kt
+++ app/src/main/java/dev/danascape/stormci/api/device/DevicesService.kt
@@ -2,9 +2,10 @@ package dev.danascape.stormci.api.device
 
 import dev.danascape.stormci.model.device.DevicesList
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface DevicesService {
     @GET("devices/devices.json")
-    fun fetchDevices(): Call<DevicesList>
+    fun fetchDevices(): Response<DevicesList>
 }
--- app/src/main/java/dev/danascape/stormci/ui/DevicesFragment.kt
+++ app/src/main/java/dev/danascape/stormci/ui/DevicesFragment.kt
@@ -1,6 +1,5 @@
 package dev.danascape.stormci.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,17 +7,14 @@ import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.github.ybq.android.spinkit.sprite.Sprite
-import com.github.ybq.android.spinkit.style.WanderingCubes
 import dev.danascape.stormci.R
 import dev.danascape.stormci.adaptor.device.DevicesListAdaptor
 import dev.danascape.stormci.api.client.GithubAPIClient
 import dev.danascape.stormci.api.device.DevicesService
 import dev.danascape.stormci.model.device.Devices
-import dev.danascape.stormci.model.device.DevicesList
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class DevicesFragment : Fragment(R.layout.fragment_devices) {
 
@@ -34,7 +30,7 @@ class DevicesFragment : Fragment(R.layout.fragment_devices) {
 
         val layoutManager = LinearLayoutManager(context)
         recyclerView = requireView().findViewById(R.id.rvDevices)
-	    progressBar = requireView().findViewById(R.id.pbDevice)
+        progressBar = requireView().findViewById(R.id.pbDevice)
         recyclerView.layoutManager = layoutManager
         mAdapter = activity?.let { DevicesListAdaptor(it, mDevices, R.layout.devices_item) }
         recyclerView.adapter = mAdapter
@@ -46,24 +42,16 @@ class DevicesFragment : Fragment(R.layout.fragment_devices) {
     }
 
     private fun fetchDevicesList() {
-        val call = mApiService!!.fetchDevices()
-
-        call.enqueue(object : Callback<DevicesList> {
-
-            @SuppressLint("NotifyDataSetChanged")
-            override fun onResponse(call: Call<DevicesList>, response: Response<DevicesList>) {
-                progressBar?.visibility = View.GONE
-                Log.d("StormCI", "Total Devices Fetched: " + response.body()!!.devices!!.size)
-                val Response = response.body()
-                if (Response != null) {
-                    mDevices.addAll(Response.devices!!)
+        GlobalScope.launch(Dispatchers.Main) {
+            val response = mApiService?.fetchDevices()
+            if(response!!.isSuccessful) {
+                    progressBar?.visibility = View.GONE
+                    mDevices.addAll(response.body()!!.devices!!)
                     mAdapter!!.notifyDataSetChanged()
                     mDevices=ArrayList<Devices>()
-                }
+            } else {
+                Log.d("StormCI", "Failed to fetch devices")
             }
-            override fun onFailure(call: Call<DevicesList>, t: Throwable) {
-                Log.d("StormCI", "Failed to download JSON")
-            }
-        })
+        }
     }
 }
