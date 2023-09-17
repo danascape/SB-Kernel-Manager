25c4c33c22e9d51b140db5f6dc67c8d48eacd073
Pratyush Singh <aries.pratyush@gmail.com>
2023-09-18 01:22:48 +0530

    feat #17: search bar in device fragment

--- app/src/main/java/dev/danascape/stormci/adapters/device/DeviceListAdapter.kt
+++ app/src/main/java/dev/danascape/stormci/adapters/device/DeviceListAdapter.kt
@@ -1,6 +1,5 @@
 package dev.danascape.stormci.adapters.device
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -10,49 +9,54 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import dev.danascape.stormci.R
 import dev.danascape.stormci.databinding.DevicesItemBinding
 import dev.danascape.stormci.models.device.Device
 
-class DeviceListAdapter(val context: Context) : RecyclerView.Adapter<DeviceListAdapter.DeviceViewHolder>() {
+class DeviceListAdapter(val context: Context) :
+    RecyclerView.Adapter<DeviceListAdapter.DeviceViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceViewHolder {
         val binding = DevicesItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return DeviceViewHolder(binding)
     }
 
-    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) {
         holder.bind(differ.currentList[position], context)
-
-//        holder.containerView.setOnClickListener {
-//            Toast.makeText(context, "Link to download the kernel", Toast.LENGTH_SHORT).show();
-//        }
     }
 
     override fun getItemCount(): Int {
         return differ.currentList.size
     }
 
-    class DeviceViewHolder(val binding: DevicesItemBinding) :
+    class DeviceViewHolder(private val binding: DevicesItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        @SuppressLint("SetTextI18n")
         fun bind(device: Device, context: Context) {
             val intentDownload = Intent(Intent.ACTION_VIEW, Uri.parse(device.link))
 
             binding.tvName.text = device.name
-            binding.tvMaintainer.text = "Maintainer: ${device.maintainer}"
-            binding.tvDate.text = "Release Date: ${device.date}"
-            binding.tvVersion.text = "Latest Version: ${device.version}"
+            binding.tvMaintainer.text =
+                context.getString(R.string.maintainer) + ": ${device.maintainer}"
+            binding.tvDate.text = context.getString(R.string.release_date) + ":  ${device.date}"
+            binding.tvVersion.text =
+                context.getString(R.string.latest_version) + ": ${device.version}"
             if (!device.image.isNullOrBlank()) {
                 Picasso.get()
                     .load(device.image)
                     .into(binding.imgProfile)
-             }
+            }
 
             binding.cvDeviceView.setOnClickListener {
                 context.startActivity(intentDownload)
             }
         }
+    }
+
+    fun setFilteredList(list: ArrayList<Device>) {
+        val newListDiffer = AsyncListDiffer(this, diffUtilCallback)
+        newListDiffer.submitList(list)
+        differ = newListDiffer
+        notifyDataSetChanged()
     }
 
     private val diffUtilCallback = object : DiffUtil.ItemCallback<Device>() {
@@ -65,5 +69,5 @@ class DeviceListAdapter(val context: Context) : RecyclerView.Adapter<DeviceListA
         }
 
     }
-    val differ = AsyncListDiffer(this, diffUtilCallback)
+    var differ = AsyncListDiffer(this, diffUtilCallback)
 }
--- app/src/main/java/dev/danascape/stormci/ui/fragments/device/DeviceFragment.kt
+++ app/src/main/java/dev/danascape/stormci/ui/fragments/device/DeviceFragment.kt
@@ -3,14 +3,16 @@ package dev.danascape.stormci.ui.fragments.device
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import dev.danascape.stormci.R
 import dev.danascape.stormci.adapters.device.DeviceListAdapter
-import dev.danascape.stormci.adapters.team.TeamListAdapter
 import dev.danascape.stormci.databinding.FragmentDevicesBinding
+import dev.danascape.stormci.models.device.Device
 import dev.danascape.stormci.repository.NetworkResponse
 
 @AndroidEntryPoint
@@ -24,11 +26,12 @@ class DeviceFragment : Fragment(R.layout.fragment_devices) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDevicesBinding.bind(view)
-//        mAdapter = DeviceListAdapter()
         mAdapter = activity?.let { DeviceListAdapter(it) }
-        val layoutManager = LinearLayoutManager(context)
-        binding.rvDevices.layoutManager = layoutManager
-        binding.rvDevices.adapter = mAdapter
+
+        binding.rvDevices.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = mAdapter
+        }
         binding.pbDevice.isIndeterminate
         viewModel.deviceList.observe(viewLifecycleOwner) {
             when (it) {
@@ -41,6 +44,38 @@ class DeviceFragment : Fragment(R.layout.fragment_devices) {
                     binding.pbDevice.visibility = View.GONE
                     mAdapter?.differ?.submitList(it.data)
                 }
+            }
+        }
+
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                filterList(newText)
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+        })
+    }
+
+    private fun filterList(text: String) {
+        if (text.isBlank()) {
+            mAdapter?.differ?.submitList(viewModel.deviceList.value?.data ?: emptyList())
+        } else {
+            val filteredList: ArrayList<Device> = arrayListOf()
+            for (item in mAdapter?.differ?.currentList!!) {
+                if (item.name?.contains(text, ignoreCase = true) == true) {
+                    filteredList.add(item)
+                }
+            }
+
+            if (filteredList.isEmpty()) {
+                Toast.makeText(requireContext(), "No Data Found", Toast.LENGTH_SHORT).show()
+            } else {
+                mAdapter!!.setFilteredList(filteredList)
             }
         }
     }
